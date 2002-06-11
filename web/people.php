8c02aab142a96a089afba9ff4f82db9846bd8458
nonchocoboy <nonchocoboy@50dcfde6-9711-4529-806c-7900125e0fc8>
2002-06-11 03:09:30 +0000

    Added the people page.
    git-svn-id: file:///desktop/SourceForge%20Backup/gltext/svnrepos/trunk@17 50dcfde6-9711-4529-806c-7900125e0fc8

---
+++ web/people.php
@@ -0,0 +1,41 @@
+<?php
+// $Id: people.php,v 1.1 2002-06-11 03:09:30 nonchocoboy Exp $
+include 'common.inc';
+layoutHeader('people', 'people');
+
+function person($name, $link, $title)
+{
+   return <<<EOD
+<tr>
+ <td class="person"><b><a href="$link">$name</a></b></td>
+ <td class="person">$title</td>
+</tr>
+EOD;
+}
+
+function personList($people)
+{
+   echo "<table class=\"person_list\">\n";
+   echo join("", $people);
+   echo "</table>\n";
+}
+
+?>
+
+<h1>People</h1>
+<p>
+GLText is developed by the following people. Be nice to them. ;) Remember that
+the best way to ask questions is to email the mailing list,
+<a href="mailto:gltext-devel@lists.sourceforge.net">gltext-devel@lists.sourceforge.net</a>,
+rather than a developer directly.
+</p>
+
+<?php
+
+personList(array(
+   person('Ben Scott', 'mailto:nonchocoboy@users.sourceforge.net',
+      'Developer and Project Manager')
+));
+
+layoutFooter();
+?>
